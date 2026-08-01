1cadcafb6e69556318fad2232eb2941c7051f4a1
Sumon Kayal <57747396+Sumon-Kayal@users.noreply.github.com>
2026-08-01 13:40:43 +0530

    Add copied files to Development-3 branch

---
+++ src/main/java/com/sumon/bundleapp/installer/billing/BillingProduct.java
@@ -0,0 +1,25 @@
+package com.sumon.bundleapp.installer.billing;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public interface BillingProduct {
+
+    @NonNull
+    String getTitle();
+
+    @Nullable
+    String getDescription();
+
+    @Nullable
+    String getIconUrl();
+
+    @NonNull
+    String getPrice();
+
+    @NonNull
+    String getId();
+
+    boolean isPurchased();
+
+}
